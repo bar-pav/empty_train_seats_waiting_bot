7bf1c9ed7e8b51f9b91e52b5fe5c871986d77252
Pavel <pavelb7c8@gmail.com>
2024-02-11 23:14:55 +0300

    add FSM

--- my_bot.py
+++ my_bot.py
@@ -1,8 +1,9 @@
 import asyncio
 from datetime import datetime
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command, CommandObject
-from aiogram import F
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command, CommandObject, StateFilter
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from config_setup import config
@@ -13,9 +14,10 @@ bot = Bot(token=config["TELEGRAM_TOKEN"])
 dp = Dispatcher()
 
 INTERVAL = 10
-status = None
-wait = False
-counter = 0
+
+
+class WaitSeats(StatesGroup):
+    wait = State()
 
 
 @dp.message(Command("test"))
@@ -37,36 +39,45 @@ async def cmd_trains(message: types.Message, command: CommandObject):
     await message.answer(res)
 
 
-@dp.message(Command("wait"))
-async def cmd_wait(message: types.Message, command: CommandObject):
+@dp.message(StateFilter(None), Command("wait"))
+async def cmd_wait(message: types.Message, command: CommandObject, state: FSMContext):
     """ :arg:  departure_station, arrival_station, departure_date, train_number=None, tickets_count=None"""
 
-    global wait, status, counter
-    wait = True
+    await state.set_state(WaitSeats.wait)
     started = f"Started at {datetime.now().strftime('%H:%M:%S, %d.%m.%Y')}"
     status = [started, None, None]
+    await state.update_data(wait=True)
     args = command.args.split()
     args[2] = args[2][0:4] + '-' + args[2][4:6] + '-' + args[2][6:8]
-    while wait:
-        res = await find_tickets(*args, message=message)
-        print(res)
+    counter = 0
+    while (await state.get_data()).get('wait'):
+        # res = await find_tickets(*args, message=message)
+        res = await find_tickets(*args)
+        # print(res)
         counter += 1
         status[1] = f"Last check at {datetime.now().strftime('%H:%M:%S, %d.%m.%Y')}"
         status[2] = f"Counter: {counter}"
+        await state.update_data(status=status)
+        # print(status)
         await asyncio.sleep(INTERVAL)
 
 
 @dp.message(Command("status"))
-async def cmd_status(message: types.Message):
-    await message.answer('\n'.join(status) if status else 'Not started')
+async def cmd_status(message: types.Message, state: FSMContext):
+    print(state)
+    if (await state.get_state()) == "WaitSeats:wait":
+        print((await state.get_data()).get("status"))
+        await message.answer(str((await state.get_data()).get("status")))
+    # await message.answer('\n'.join(status) if status else 'Not started')
 
 
-@dp.message(Command("stop"))
-async def cmd_stop(message: types.Message):
-    global wait, status, counter
+@dp.message(WaitSeats.wait, Command("stop"))
+async def cmd_stop(message: types.Message, state: FSMContext):
+    # global wait, status, counter
     wait = False
     status = None
     counter = 0
+    await state.clear()
     print('stopped')
     await message.answer('stopped')
 
