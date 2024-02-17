c4ef4b88d3c06c35a092e1a2ee73bd48d47ce673
Pavel <myemail>
2024-02-18 00:35:21 +0300

    .

--- my_bot.py
+++ my_bot.py
@@ -3,13 +3,21 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from config_setup import config
 
+from time import sleep
+
 bot = Bot(token=config["TELEGRAM_TOKEN"])
 dp = Dispatcher()
 
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    # await message.answer("Hello")
+    await message.answer("Let's find empty seats!")
+
+
+@dp.message(Command("2"))
+async def cmd_2(message: types.Message):
+    await message.answer("?")
 
 
 async def main():
